897573cdb4acc680eab13a4f47951a0d39d3e171
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-13 20:16:08 +0900

    Feature: チャット読み上げ部分の完成

--- src/main/java/aaarsalmon/Chat2BouyomiTCP.java
+++ src/main/java/aaarsalmon/Chat2BouyomiTCP.java
@@ -7,6 +7,14 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+
 @Mod("chat2bouyomitcp")
 public class Chat2BouyomiTCP {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -17,7 +25,46 @@ public class Chat2BouyomiTCP {
 
 	@SubscribeEvent
 	public void onServerChat(ServerChatEvent event) {
-		LOGGER.debug("コメントされたねぇ");
-		LOGGER.info(event.getMessage());
+		send(event.getMessage());
+	}
+
+	public static void bouyomi(String message) {
+
+	}
+
+	public static void send(String message) {
+		Socket socket = null;
+		OutputStream out = null;
+		try {
+			socket = new Socket("127.0.0.1", 50001);
+			out = socket.getOutputStream();
+
+			byte[] messageData = message.getBytes(StandardCharsets.UTF_8);
+			byte[] data = new byte[15 + messageData.length];
+			ByteBuffer bb = ByteBuffer.wrap(data);
+			bb.order(ByteOrder.LITTLE_ENDIAN);
+			bb.putShort(0, (short) 0x0001);
+			bb.putShort(2, (short) 0xFFFF); // 速度
+			bb.putShort(4, (short) 0xFFFF); // 音程
+			bb.putShort(6, (short) 0xFFFF); // 音量
+			bb.putShort(8, (short) 0x0000); // 声質
+			bb.putChar(10, (char) 0x00); // 文字コード UTF-8
+			bb.putInt(11, messageData.length); // 文字列の長さ
+			System.arraycopy(messageData, 0, data, 15, messageData.length);
+			out.write(data);
+
+			out.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
